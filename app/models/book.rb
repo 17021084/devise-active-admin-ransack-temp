75098ac25c3b68a797850d25f7c1d72072184f7b
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2024-01-04 22:18:28 +0700

    custom css, custom footer

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
-    def self.ransackable_attributes(auth_object = nil)
-        ["author", "created_at", "id", "id_value", "price", "title", "updated_at"]
-    end
+	def self.ransackable_attributes(auth_object = nil)
+		["author", "created_at", "id", "id_value", "price", "title", "updated_at"]
+	end
+
+	def self.recent number 
+		Book.last(number)
+	end
 end
